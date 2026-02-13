e56f84b896140e9b479b7e66348dd9a2670f7cef
saur0w <sthapliyal085@gmail.com>
2026-02-13 15:21:09 +0530

    "Landing Page GSAP"

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -2,12 +2,45 @@
 
 import styles from './style.module.scss';
 import RoundedButton from "@/ui/Button/index";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import Text from "@/ui/Text/index";
+
+gsap.registerPlugin(useGSAP);
 
 export default function Landing() {
+
+    const landingRef = useRef<HTMLDivElement>(null);
+    const headerRef = useRef<HTMLHeadingElement>(null);
+    const headRef = useRef<HTMLDivElement>(null);
+
+    useGSAP(() => {
+        const introT1 = gsap.timeline();
+
+        introT1
+            .set(headRef.current, {
+                opacity: 1
+            })
+            .from(headerRef.current, {
+                scale: 3,
+                opacity: 0,
+                duration: 1,
+                ease: "power4.in",
+                delay: 0.3,
+                stagger: 1,
+        })
+    }, { scope: landingRef });
+
     return (
-        <section className={styles.landing}>
+        <section className={styles.landing} ref={landingRef}>
+            <div ref={headRef} className="opacity-0">
+                <h1 className={styles.header} ref={headerRef}>Own the Moment</h1>
+            </div>
             <div>
-                <h1 className={styles.header}>Own the Moment</h1>
+                <Text>
+                    <p>50ml • Long-lasting • Crafted with rare notes</p>
+                </Text>
             </div>
             <div className={styles.button}>
                 <RoundedButton><p>Shop now</p></RoundedButton>
