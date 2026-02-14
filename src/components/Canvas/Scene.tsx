e4ee2acc0f2f851454124fd0dceb80037f4734d8
saur0w <sthapliyal085@gmail.com>
2026-02-14 14:11:52 +0530

    "Controlling Second Model with GSAP"

--- src/components/Canvas/Scene.tsx
+++ src/components/Canvas/Scene.tsx
@@ -7,7 +7,7 @@ import { View } from "@react-three/drei";
 export default function ViewCanvas() {
     return (
         <div className={styles.model}>
-            <Canvas camera={{ position: [0, 0, 5], fov: 45 }}>
+            <Canvas camera={{ position: [0, 0, 5], fov: 45 }} gl={{ antialias: true }}>
                 <View.Port />
             </Canvas>
         </div>
--- src/components/Landing/Scene.tsx
+++ src/components/Landing/Scene.tsx
@@ -6,17 +6,36 @@ import { Environment } from "@react-three/drei";
 import { Float } from "@react-three/drei";
 import { useRef } from "react";
 import { Group } from "three";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
+
+gsap.registerPlugin(useGSAP);
 
 export default function Scene() {
     const modelRef = useRef<Group>(null);
+    const perfumeRef = useRef<Group>(null);
+    const serumRef = useRef<Group>                                                                                              (null);
 
+
+    useGSAP(() => {
+        if (!modelRef.current || !perfumeRef.current || !serumRef.current) return;
+
+        gsap.set(serumRef.current.position, {
+            y: -5,
+        });
+    }, { scope: modelRef });
     return (
         <group>
             <Float ref={modelRef} floatIntensity={0.2}>
-                <Model />
-                <Serum />
+                <group ref={perfumeRef}>
+                    <Model />
+                </group>
+
+                <group ref={serumRef} >
+                    <Serum />
+                </group>
             </Float>
-            <Environment preset="dawn" />
+            <Environment preset="dawn" environmentIntensity={0.5} />
             <ambientLight />
         </group>
     )
